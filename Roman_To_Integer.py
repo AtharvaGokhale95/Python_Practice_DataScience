96417bdcf07a047b76ce6072bb6177a5cc6b3954
Atharva Gokhale <atharvagokhale95@gmailcom>
2025-07-12 02:40:10 -0400

    Adds roman_to_int.py file

---
+++ Roman_To_Integer.py
@@ -0,0 +1,25 @@
+class Solution:
+    dict = {'I':1, 'V':5, 'X':10, 'L': 50, 'C':100, 'D':500, 'M':1000}
+    
+    # def roman_To_Int_partial(self, s:str) -> int:
+    #     val = 0
+    #     for char in s:
+    #         val += dict[char]
+    #     return val
+    
+    
+    def roman_To_Int_complete(self, s:str) -> int:
+        prev = 0
+        total = 0
+        for char in reversed(s):
+            val = self.dict[char]
+            if val > prev:
+               total += val
+               
+            else:
+               total -= val
+            prev = val
+        return total
+
+obj = Solution()
+print(obj.roman_To_Int_complete('IX'))
