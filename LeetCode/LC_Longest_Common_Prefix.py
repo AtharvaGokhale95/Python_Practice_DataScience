5b2edba9593207dbed44b1548fade976b6a7d69f
Atharva Gokhale <atharvagokhale95@gmailcom>
2025-07-14 00:06:27 -0400

    Update

---
+++ LeetCode/LC_Longest_Common_Prefix.py
@@ -0,0 +1,25 @@
+from typing import List
+
+# Time Complexity: O(n^2)
+class Solution:
+    def longestcommonprefix(self, strs:List[str]) -> str:
+
+        if not strs:                    # If there is not a input string, return a blank string as longest common prefix
+            return ""
+        
+        prefix = ""
+        # Start with iterating over just the first word to find the common string
+        # We will iterate upto range(len(strs[0])) as the common string could be the entire first word
+        for idx in range(len(strs[0])):
+            current_char = strs[0][idx]
+            for word in strs[1:]:
+                if idx >= len(word) or word[idx] != current_char:
+                    return prefix
+            # Add the new word[idx] after all characters match at this index
+            prefix += current_char
+        return prefix
+
+obj = Solution()
+print(obj.longestcommonprefix(["flower","flow","flight"]))
+
+# Time Complexity: O(n) - Using single for loop:
---
+++ LeetCode/LC_Palindrome.py
@@ -0,0 +1,48 @@
+'''
+Given an integer x, return true if x is a palindrome, and false otherwise.
+'''
+
+
+# Using list reversal
+class Solution:
+    def isPalindrome_reverse(self, x:int) -> bool:
+        x_list = [str(char) for char in str(x)]
+        x_list_reverse = x_list[::-1]
+        if x_list == x_list_reverse:
+            return True
+        return False
+    
+    def isPalindrome_reverse_str(self, x:int) -> bool:
+        x_str = str(x)
+        x_str_reverse = x_str[::-1]
+        if x_str == x_str_reverse:
+            return True
+        return False
+    
+    # Using while loop:
+    def isPalindrome_while(self, x):
+        x_str = str(x)                              # Type casted int to str
+        if len(x_str) == 1:
+            return True
+        
+        head = 0
+        tail = len(x_str) - 1
+        
+        while head < tail:          # This while loop ensure the comparing values are not the same and 
+                                    # We exactly keep comparing one value from the front and other from the back of the string while head < tail               
+            if x_str[head] == x_str[tail]:
+                return True
+            else:
+                return False
+            head += 1
+            tail -= 1
+            
+    
+obj = Solution()
+print(obj.isPalindrome_reverse_str(121))
+# print(obj.isPalindrome_while(1212))
+# print(obj.isPalindrome(121))
+        
+
+
+
