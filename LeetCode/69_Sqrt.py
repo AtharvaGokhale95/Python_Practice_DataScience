0dbf1563ffb782d183048008fbc01bb258dcf2da
Atharva Gokhale <atharvagokhale95@gmailcom>
2025-08-07 01:14:35 -0400

    Adds two new .py files

--- LeetCode/69_Sqrt.py
+++ LeetCode/69_Sqrt.py
@@ -1,1 +1,32 @@
-print("It is working")
+class Solution:
+    def sqrt_builtin(self, n : int) -> int:
+        return n**0.5
+    
+    def sqrt_heron(self, n : int, tolerance = 1e-10) -> int:
+        x = n
+        while True:
+            root = 0.5 * (x + n/x)
+            if abs(root - x) < tolerance:
+                return root
+            x = root
+    
+    def sqrt_binary(self, n:int) -> int:
+        if n <2:
+            return n
+        
+        low, high = 1, n
+        while low <= high:
+            mid = (low + high) // 2         # Determined the middle number between 1 and n
+            if mid * mid == n:
+                return mid
+            if mid * mid < n:
+                low = mid + 1
+            else:
+                high = mid - 1
+        return high
+            
+        
+s = Solution()
+print(s.sqrt_builtin(25))
+print(s.sqrt_heron(25))
+print(s.sqrt_binary(25))
